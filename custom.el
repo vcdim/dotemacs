2c12ab0475f51d26f53b7de915bb6ce762320d35
Qun Gu <guqun@outlook.com>
2021-01-04 00:35:12 +0800

    update org heading sizes

--- custom.el
+++ custom.el
@@ -3,9 +3,15 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-document-title ((t (:weight bold :height 2.0 :family "CMU Serif"))))
- '(outline-1 ((t (:weight bold :height 1.75 :family "CMU Serif"))))
- '(outline-2 ((t (:weight bold :height 1.5 :family "CMU Sans Serif"))))
- '(outline-3 ((t (:weight bold :height 1.25 :family "CMU Serif Upright Italic"))))
- '(outline-4 ((t (:weight bold :height 1.125 :family "CMU Bright"))))
+ '(org-document-title ((t (:weight bold :height 1.5 :family "CMU Serif"))))
+ '(outline-1 ((t (:weight bold :height 1.4 :family "CMU Serif"))))
+ '(outline-2 ((t (:weight bold :height 1.25 :family "CMU Sans Serif"))))
+ '(outline-3 ((t (:weight bold :height 1.125 :family "CMU Serif Upright Italic"))))
+ '(outline-4 ((t (:weight bold :height 1.05 :family "CMU Bright"))))
+ '(variable-pitch ((t (:family "CMU Serif")))))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  )
