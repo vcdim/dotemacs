4f96810a17fcab57b9109dd0168b6d1ab6a2c491
Qun Gu <guqun@outlook.com>
2021-01-18 12:36:36 +0800

    add centaur

--- custom.el
+++ custom.el
@@ -18,6 +18,7 @@
    [default default default italic underline success warning error])
  '(ansi-color-names-vector
    ["#2d3743" "#ff4242" "#74af68" "#dbdb95" "#34cae2" "#008b8b" "#00ede1" "#e1e1e0"])
+ '(centaur-tabs-mode t nil (centaur-tabs))
  '(fci-rule-color "#5c5e5e")
  '(highlight-indent-guides-delay 0)
  '(highlight-indent-guides-method 'character)
@@ -40,7 +41,7 @@
  '(org-latex-pdf-process
    '("xelatex -interaction nonstopmode -output-directory %o %f" "xelatex -interaction nonstopmode -output-directory %o %f" "xelatex -interaction nonstopmode -output-directory %o %f"))
  '(package-selected-packages
-   '(yasnippet-snippets yasnippet highlight-indent-guides highlight-indent-guide dashboard pdf-view-restore org-noter-pdftools org-pdftools ibuffer-vc all-the-icons-ibuffer dired-narrow dired-ranger dired-subtree dired-rainbow dired-open dired-avfs dired-filter dired-list dired-collapse dired-hacks-utils dired+ company-capf company-mode company company-lsp which-key use-package treemacs-projectile treemacs-magit treemacs-icons-dired smex rainbow-delimiters pyenv-mode org2blog org-superstar org-roam-server org-kanban org-fancy-priorities org-download lsp-ui lsp-python-ms lsp-ivy helpful exec-path-from-shell doom-themes doom-modeline dap-mode counsel cnfonts auctex all-the-icons-ivy-rich all-the-icons-ivy all-the-icons-dired))
+   '(base16-theme kaolin-themes centaur-tabs elpy yasnippet-snippets yasnippet highlight-indent-guides highlight-indent-guide dashboard pdf-view-restore org-noter-pdftools org-pdftools ibuffer-vc all-the-icons-ibuffer dired-narrow dired-ranger dired-subtree dired-rainbow dired-open dired-avfs dired-filter dired-list dired-collapse dired-hacks-utils dired+ company-capf company-mode company company-lsp which-key use-package treemacs-projectile treemacs-magit treemacs-icons-dired smex rainbow-delimiters pyenv-mode org2blog org-superstar org-roam-server org-kanban org-fancy-priorities org-download lsp-ui lsp-python-ms lsp-ivy helpful exec-path-from-shell doom-themes doom-modeline dap-mode counsel cnfonts auctex all-the-icons-ivy-rich all-the-icons-ivy all-the-icons-dired))
  '(pdf-view-midnight-colors (cons "#c5c8c6" "#1d1f21"))
  '(rustic-ansi-faces
    ["#1d1f21" "#cc6666" "#b5bd68" "#f0c674" "#81a2be" "#c9b4cf" "#8abeb7" "#c5c8c6"])
--- init.el
+++ init.el
@@ -54,8 +54,18 @@
   :init (doom-modeline-mode 1))
 (use-package rainbow-delimiters
   :hook (prog-mode . rainbow-delimiters-mode))
-(use-package doom-themes)
-;;  :init (load-theme 'doom-opera-light t))
+(use-package doom-themes
+  ;;  :init (load-theme 'doom-opera-light t)
+  )
+(use-package kaolin-themes
+  :config
+;;  (load-theme 'kaolin-dark t)
+;;  (kaolin-treemacs-theme)
+  )
+(use-package base16-theme
+  :config
+;;  (load-theme 'base16-default-dark t)
+  )
 (use-package dashboard
   :config
   (dashboard-setup-startup-hook)
@@ -72,6 +82,8 @@
 (use-package all-the-icons-ivy
   :init (add-hook 'after-init-hook 'all-the-icons-ivy-setup))
 (use-package all-the-icons-ivy-rich
+  :init (all-the-icons-ivy-rich-mode 1))
+(use-package all-the-icons-ibuffer
   :init (all-the-icons-ivy-rich-mode 1))
 (use-package all-the-icons-dired
   :init (add-hook 'dired-mode-hook 'all-the-icons-dired-mode))
@@ -175,6 +187,76 @@
 (require 'dap-python)
 
 ;; treemacs, ibuffer
+ (use-package centaur-tabs
+   :load-path "~/.emacs.d/other/centaur-tabs"
+   :config
+   (setq centaur-tabs-style "bar"
+	 centaur-tabs-height 32
+	 centaur-tabs-set-icons t
+	 centaur-tabs-set-modified-marker t
+	 centaur-tabs-show-navigation-buttons t
+	 centaur-tabs-set-bar 'under
+	 x-underline-at-descent-line t)
+   (centaur-tabs-headline-match)
+   ;; (setq centaur-tabs-gray-out-icons 'buffer)
+   ;; (centaur-tabs-enable-buffer-reordering)
+   ;; (setq centaur-tabs-adjust-buffer-order t)
+   (centaur-tabs-mode t)
+   (setq uniquify-separator "/")
+   (setq uniquify-buffer-name-style 'forward)
+   (defun centaur-tabs-buffer-groups ()
+     "`centaur-tabs-buffer-groups' control buffers' group rules.
+
+ Group centaur-tabs with mode if buffer is derived from `eshell-mode' `emacs-lisp-mode' `dired-mode' `org-mode' `magit-mode'.
+ All buffer name start with * will group to \"Emacs\".
+ Other buffer group by `centaur-tabs-get-group-name' with project name."
+     (list
+      (cond
+	;; ((not (eq (file-remote-p (buffer-file-name)) nil))
+	;; "Remote")
+	((or (string-equal "*" (substring (buffer-name) 0 1))
+	     (memq major-mode '(magit-process-mode
+				magit-status-mode
+				magit-diff-mode
+				magit-log-mode
+				magit-file-mode
+				magit-blob-mode
+				magit-blame-mode
+				)))
+	 "Emacs")
+	((derived-mode-p 'prog-mode)
+	 "Editing")
+	((derived-mode-p 'dired-mode)
+	 "Dired")
+	((memq major-mode '(helpful-mode
+			    help-mode))
+	 "Help")
+	((memq major-mode '(org-mode
+			    org-agenda-clockreport-mode
+			    org-src-mode
+			    org-agenda-mode
+			    org-beamer-mode
+			    org-indent-mode
+			    org-bullets-mode
+			    org-cdlatex-mode
+			    org-agenda-log-mode
+			    diary-mode))
+	 "OrgMode")
+	(t
+	 (centaur-tabs-get-group-name (current-buffer))))))
+   :hook
+   (dashboard-mode . centaur-tabs-local-mode)
+   (term-mode . centaur-tabs-local-mode)
+   (calendar-mode . centaur-tabs-local-mode)
+   (org-agenda-mode . centaur-tabs-local-mode)
+   (helpful-mode . centaur-tabs-local-mode)
+   :bind
+   ("C-<prior>" . centaur-tabs-backward)
+   ("C-<next>" . centaur-tabs-forward)
+   ("C-c t s" . centaur-tabs-counsel-switch-group)
+   ("C-c t p" . centaur-tabs-group-by-projectile-project)
+   ("C-c t g" . centaur-tabs-group-buffer-groups))
+
 (setq dired-guess-shell-alist-user '(("\\.pdf\\'" "open")
                                    ("\\.doc\\'" "open")
                                    ("\\.docx\\'" "open")
@@ -264,9 +346,6 @@
         ("C-x t M-t" . treemacs-find-tag)))
 (use-package treemacs-projectile
   :after treemacs projectile)
-(use-package treemacs-icons-dired
-  :after treemacs dired
-  :config (treemacs-icons-dired-mode))
 (use-package treemacs-magit
   :after treemacs magit)
 
@@ -323,24 +402,24 @@
   (setq org-fancy-priorities-list
 	'((?A . "❗")(?B . "⬆")(?C . "⬇")(?D . "☕")
 	  (?1 . "⚡")(?2 . "⮬")(?3 . "⮮")(?4 . "☕")(?I . "Important"))))
-(use-package pdf-tools
-  :defer t
-  :commands (pdf-view-mode pdf-tools-install)
-  :mode ("\\.[pP][dD][fF]" . pdf-view-mode)
-  :magic ("%PDF" . pdf-view-mode)
-  :config (pdf-tools-install)
-  )
-(use-package pdf-view-restore
-  :after pdf-tools
-  :config
-  (add-hook 'pdf-view-mode-hook 'pdf-view-restore-mode))
-(use-package org-pdftools
-  :hook (org-mode . org-pdftools-setup-link))
-(use-package org-noter-pdftools
-  :after org-noter
-  :config
-  (with-eval-after-load 'pdf-annot
-    (add-hook 'pdf-annot-activate-handler-functions #'org-noter-pdftools-jump-to-note)))
+;; (use-package pdf-tools
+;;   :defer t
+;;   :commands (pdf-view-mode pdf-tools-install)
+;;   :mode ("\\.[pP][dD][fF]" . pdf-view-mode)
+;;   :magic ("%PDF" . pdf-view-mode)
+;;   :config (pdf-tools-install :no-query)
+;;   )
+;; (use-package pdf-view-restore
+;;   :after pdf-tools
+;;   :config
+;;   (add-hook 'pdf-view-mode-hook 'pdf-view-restore-mode))
+;; (use-package org-pdftools
+;;   :hook (org-mode . org-pdftools-setup-link))
+;; (use-package org-noter-pdftools
+;;   :after org-noter
+;;   :config
+;;   (with-eval-after-load 'pdf-annot
+;;     (add-hook 'pdf-annot-activate-handler-functions #'org-noter-pdftools-jump-to-note)))
 
 (defface org-checkbox-done-text
   '((t (:foreground "#71696A" :strike-through t)))
@@ -376,6 +455,25 @@
   (when (org-export-derived-backend-p backend 'latex)
     (replace-regexp-in-string "\x200B" "{}" text)))
 
+;; hideshow
+(defun toggle-selective-display (column)
+  (interactive "P")
+  (set-selective-display
+   (or column
+       (unless selective-display
+         (1+ (current-column))))))
+(defun toggle-hiding (column)
+  (interactive "P")
+  (if hs-minor-mode
+      (if (condition-case nil
+              (hs-toggle-hiding)
+            (error t))
+          (hs-show-all))
+    (toggle-selective-display column)))
+(global-set-key (kbd "C-+") 'toggle-hiding)
+(global-set-key (kbd "C-\\") 'toggle-selective-display)
+(add-hook 'prog-mode-hook #'hs-minor-mode)
+
 ;; key bindings
 (global-set-key (kbd "<home>") 'beginning-of-line)
 (global-set-key (kbd "<end>") 'end-of-line)
@@ -390,8 +488,8 @@
 (setenv "LC_ALL" "en_US.UTF-8")
 (setenv "LC_CTYPE" "en_US.UTF-8")
 
-(setq url-proxy-services '(("http" . "127.0.0.1:7890")
-			   ("https" . "127.0.0.1:7890")))
+;; (setq url-proxy-services '(("http" . "127.0.0.1:7890")
+;; 			   ("https" . "127.0.0.1:7890")))
 
 ;; make init.el clean
 (setq custom-file (concat user-emacs-directory "custom.el"))
