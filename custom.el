79f745c4a687f65aa8f8bcdd35398221c326a023
Qun Gu <guqun@outlook.com>
2021-03-28 21:22:05 +0800

    remove cnfonts!

--- custom.el
+++ custom.el
@@ -9,4 +9,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(emojify-emoji-set "openmoji-v13-0"))
+ '(org-agenda-files
+   '("~/SynologyDrive/org/gcal.org" "~/SynologyDrive/org/inbox.org" "~/SynologyDrive/org/habits.org" "~/SynologyDrive/org/birthdays.org" "/Users/gq/SynologyDrive/org/journal/2021-03.org")))
