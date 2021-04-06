8222acc57fce50e67d9dfc7c57b90ffbf883ff7b
Qun Gu <guqun@outlook.com>
2021-04-06 22:16:11 +0800

    changed a lot

--- custom.el
+++ custom.el
@@ -9,5 +9,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(diredfl-global-mode t nil (diredfl))
  '(org-agenda-files
-   '("~/SynologyDrive/org/gcal.org" "~/SynologyDrive/org/inbox.org" "~/SynologyDrive/org/habits.org" "~/SynologyDrive/org/birthdays.org" "/Users/gq/SynologyDrive/org/journal/2021-03.org")))
+   '("~/SynologyDrive/org/gcal.org" "~/SynologyDrive/org/inbox.org" "~/SynologyDrive/org/habits.org" "~/SynologyDrive/org/archive/tasks.org" "~/SynologyDrive/org/birthdays.org" "/Users/gq/SynologyDrive/org/journal/2021-04.org")))
