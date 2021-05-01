ae90d8d0c1004fab2612d5ff924979e415e64169
Gu <davidqun@3c22fb775741.ant.amazon.com>
2021-04-30 17:55:18 -0700

    add mac key bindings

--- init.el
+++ init.el
@@ -79,7 +79,34 @@
 ;; 
 (when (eq system-type 'darwin)
   (setq mac-option-modifier 'meta)
-  (global-unset-key (kbd "C-M-SPC")))
+  (setq mac-command-modifier 'super) 
+  (global-unset-key (kbd "C-M-SPC"))
+  (global-set-key (kbd "s-c") 'kill-ring-save)
+  (global-set-key (kbd "s-v") 'yank)
+  (global-set-key (kbd "s-x") 'kill-region)
+  (global-set-key (kbd "s-a") 'mark-whole-buffer)
+  (global-set-key (kbd "s-z") 'undo)
+  (global-set-key (kbd "s-f") 'isearch-forward)
+  (global-set-key (kbd "s-g") 'isearch-repeat-forward)
+  (global-set-key (kbd "s-o") 'find-file)
+  (global-set-key (kbd "s-o") 'mac-open-file)
+  (global-set-key (kbd "s-n") 'find-file)
+  (global-set-key (kbd "s-s") 'save-buffer)
+  (global-set-key (kbd "s-S") 'mac-save-file-as) 
+  (global-set-key (kbd "s-p") 'mac-preview) ; requires mac-preview
+  (global-set-key (kbd "s-w") 'kill-buffer)
+  (global-set-key (kbd "s-m") 'iconify-frame)
+  (global-set-key (kbd "s-q") 'save-buffers-kill-emacs)
+  (global-set-key (kbd "s-.") 'keyboard-quit)
+  (global-set-key (kbd "s-l") 'goto-line)
+  (global-set-key (kbd "s-k") 'kill-buffer)
+  (global-set-key (kbd "s-<up>")    'beginning-of-buffer)
+  (global-set-key (kbd "s-<down>")  'end-of-buffer)
+  (global-set-key (kbd "s-<left>")  'beginning-of-line)
+  (global-set-key (kbd "s-<right>") 'end-of-line)
+  (global-set-key [(meta down)]     'forward-paragraph)
+  (global-set-key [(meta up)]       'backward-paragraph)
+  )
 
 ;; 不需要备份文件
 (setq make-backup-files nil)
@@ -92,6 +119,7 @@
 ;; 系统代理
 ;; (setq url-proxy-services '(("http" . "127.0.0.1:7890")
 ;;                            ("https" . "127.0.0.1:7890")))
+(setq org-image-actual-width 800)
 
 (use-package general
   :config
